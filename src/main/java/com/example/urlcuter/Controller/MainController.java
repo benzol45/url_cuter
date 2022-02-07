0381886911013a253ef726376d18a881ba6a2975
Михаил Калинин <benzol.45@gmail.com>
2022-02-07 17:53:34 +0500

    add cookie

--- src/main/java/com/example/urlcuter/Controller/MainController.java
+++ src/main/java/com/example/urlcuter/Controller/MainController.java
@@ -3,9 +3,16 @@ package com.example.urlcuter.Controller;
 import com.example.urlcuter.Service.UrlService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
 
 
 @Controller
@@ -19,7 +26,14 @@ public class MainController {
     }
 
     @GetMapping("/{cutUrl}")
-    public String getMain(@PathVariable String cutUrl){
+    public String getMain(@PathVariable String cutUrl, @CookieValue(name = "ClientID", defaultValue = "") String ClientID, HttpServletResponse httpServletResponse){
+        if (ClientID.equals("")) {
+            Cookie cookie = new Cookie("ClientID", UUID.randomUUID().toString());
+            cookie.setMaxAge(1000);
+            httpServletResponse.addCookie(cookie);
+        } else
+            System.out.println(ClientID);
+
         return "redirect://" + urlService.getFullUrlByCutUrl(cutUrl);
     }
 }
