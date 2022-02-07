ecde86934174454df10e8eeba45c6299449278ca
Михаил Калинин <benzol.45@gmail.com>
2022-02-07 13:58:28 +0500

    Add main controller

---
+++ src/main/java/com/example/urlcuter/Controller/MainController.java
@@ -0,0 +1,15 @@
+package com.example.urlcuter.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+    @GetMapping("/")
+    public String getMain(){
+        return "redirect:/google.com";
+    }
+}
