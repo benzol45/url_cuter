c233bc064c065588df662f2acb16f69b9bf8e481
Михаил Калинин <benzol.45@gmail.com>
2022-02-07 13:51:37 +0500

    Open project

---
+++ src/main/java/com/example/urlcuter/UrlCuterApplication.java
@@ -0,0 +1,13 @@
+package com.example.urlcuter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlCuterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlCuterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/urlcuter/UrlCuterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.urlcuter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlCuterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
