f863340ca632428d44db506276fdd4b0da98c4e8
Михаил Калинин <benzol.45@gmail.com>
2022-02-07 15:13:27 +0500

    convertToMVC

--- src/main/java/com/example/urlcuter/Controller/MainController.java
+++ src/main/java/com/example/urlcuter/Controller/MainController.java
@@ -6,13 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class MainController {
 
     @GetMapping
-    @ResponseBody
     public String getMain(){
-        return "_redirect:/google.com";
+        return "redirect:/google.com";
     }
 }
