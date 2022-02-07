eed270e25261a3458de0347549e681c450d59147
Михаил Калинин <benzol.45@gmail.com>
2022-02-07 15:06:43 +0500

    convertToRest

--- src/main/java/com/example/urlcuter/Controller/MainController.java
+++ src/main/java/com/example/urlcuter/Controller/MainController.java
@@ -3,13 +3,16 @@ package com.example.urlcuter.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/")
 public class MainController {
-    @GetMapping()
+
+    @GetMapping
+    @ResponseBody
     public String getMain(){
-        return "redirect:/google.com";
+        return "_redirect:/google.com";
     }
 }
